a45c9254259624906fac015081db1b029e8551d3
Chabam <fchabot1337@gmail.com>
2021-12-13 00:45:35 -0500

    day 3 pt.2 -> day 5

--- 3.py
+++ 3.py
@@ -1,4 +1,5 @@
 from aoc_utils import compute_answer, PuzzleInput
+from typing import List
 
 
 def part1(puzzle_input: PuzzleInput):
@@ -13,8 +14,8 @@ def part1(puzzle_input: PuzzleInput):
     for bit_pos in range(binary_length):
         amount_zero = 0
 
-        for report in range(report_amount):
-            if bit_array[report][bit_pos] == 0:
+        for report_idx in range(report_amount):
+            if bit_array[report_idx][bit_pos] == 0:
                 amount_zero += 1
 
         if amount_zero > report_amount / 2:
@@ -29,4 +30,47 @@ def part1(puzzle_input: PuzzleInput):
     print(epsilon_rate * gamma_rate)
 
 
-compute_answer(3, part1, None)
+def part2(puzzle_input: PuzzleInput):
+    bit_array = list(map(lambda bit_row: [int(bit)
+                                          for bit in bit_row], puzzle_input))
+
+    def filter_numbers_recursively(current_array: List[List[int]], bit_pos: int, is_o2_generator: bool):
+        report_amount = len(current_array)
+
+        if report_amount == 1:
+            return "".join([str(bit) for bit in current_array[0]])
+
+        amount_zero = 0
+        for report_idx in range(report_amount):
+            if current_array[report_idx][bit_pos] == 0:
+                amount_zero += 1
+
+        amount_for_majority = report_amount / 2
+
+        bit_val_to_filter = None
+        if amount_zero > amount_for_majority:
+            bit_val_to_filter = 0
+        elif amount_zero < amount_for_majority:
+            bit_val_to_filter = 1
+        else:
+            bit_val_to_filter = 1
+
+        # invert the filter if not o2 generator
+        if not is_o2_generator:
+            bit_val_to_filter = 1 - bit_val_to_filter
+
+        new_array = []
+        for report_idx in range(report_amount):
+            if current_array[report_idx][bit_pos] == bit_val_to_filter:
+                new_array.append(current_array[report_idx])
+
+        return filter_numbers_recursively(
+            new_array, bit_pos + 1, is_o2_generator)
+
+    o2_generator = int(filter_numbers_recursively(bit_array, 0, True), 2)
+    co2_scrubber = int(filter_numbers_recursively(bit_array, 0, False), 2)
+
+    print(o2_generator * co2_scrubber)
+
+
+compute_answer(3, part1, part2)
