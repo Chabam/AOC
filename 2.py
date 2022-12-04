b26b6ed970e4f24fa3fcb16b2672c498aafacdec
Chabam <fchabot1337@gmail.com>
2022-12-04 17:35:53 -0500

    Day part 2

--- 2.py
+++ 2.py
@@ -1,3 +1,4 @@
+from typing import Dict, Union
 from aoc_utils import compute_answer, PuzzleInput
 from enum import Enum, auto
 
@@ -14,7 +15,7 @@ class Outcome(Enum):
     LOSS = 0
 
 
-RPS_MAPPING_PART1 = {
+RPS_MAPPING_PART1: Dict[str, RPS] = {
     "A": RPS.ROCK,
     "B": RPS.PAPER,
     "C": RPS.SCISSORS,
@@ -24,23 +25,58 @@ RPS_MAPPING_PART1 = {
 }
 
 
-def get_score_part1(rps_round: str) -> int:
-    oponent, me = [RPS_MAPPING_PART1[value] for value in rps_round.split(" ")]
-    match oponent, me:
-        case RPS.SCISSORS, RPS.ROCK:
-            return Outcome.WIN.value + RPS.ROCK.value
-        case RPS.PAPER, RPS.SCISSORS:
-            return Outcome.WIN.value + RPS.SCISSORS.value
-        case RPS.ROCK, RPS.PAPER:
-            return Outcome.WIN.value + RPS.PAPER.value
-        case _, _ if me == oponent:
-            return me.value + Outcome.DRAW.value
-        case _, _:
-            return me.value
-
-
 def part1(rps_rounds: PuzzleInput):
-    print(sum([get_score_part1(rps_round) for rps_round in rps_rounds]))
+    def get_score(rps_round: str) -> int:
+        oponent, me = [RPS_MAPPING_PART1[value]
+                       for value in rps_round.split(" ")]
+        match oponent, me:
+            case RPS.SCISSORS, RPS.ROCK:
+                return Outcome.WIN.value + RPS.ROCK.value
+            case RPS.PAPER, RPS.SCISSORS:
+                return Outcome.WIN.value + RPS.SCISSORS.value
+            case RPS.ROCK, RPS.PAPER:
+                return Outcome.WIN.value + RPS.PAPER.value
+            case _, _ if me == oponent:
+                return me.value + Outcome.DRAW.value
+            case _, _:
+                return me.value
+
+    print(sum([get_score(rps_round) for rps_round in rps_rounds]))
 
 
-compute_answer(2, part1, None)
+RPS_MAPPING_PART2: Dict[str, Union[RPS, Outcome]] = {
+    "A": RPS.ROCK,
+    "B": RPS.PAPER,
+    "C": RPS.SCISSORS,
+    "X": Outcome.LOSS,
+    "Y": Outcome.DRAW,
+    "Z": Outcome.WIN,
+}
+
+
+def part2(rps_rounds: PuzzleInput):
+    def get_opposite(rps: RPS) -> RPS:
+        match rps:
+            case RPS.ROCK:
+                return RPS.PAPER
+            case RPS.PAPER:
+                return RPS.SCISSORS
+            case RPS.SCISSORS:
+                return RPS.ROCK
+
+    def get_score(rps_round: str) -> int:
+        oponent, outcome = [RPS_MAPPING_PART2[value]
+                            for value in rps_round.split(" ")]
+
+        match outcome:
+            case Outcome.DRAW:
+                return oponent.value + Outcome.DRAW.value
+            case Outcome.WIN:
+                return get_opposite(oponent).value + Outcome.WIN.value
+            case Outcome.LOSS:
+                return get_opposite(get_opposite(oponent)).value
+
+    print(sum([get_score(rps_round) for rps_round in rps_rounds]))
+
+
+compute_answer(2, part1, part2)
